b71c9159d5a115b346cc9772f0ab5be9131e06dd
Mason <masonwillman0@gmail.com>
2023-02-07 11:43:05 -0500

    Lab5 Submission

--- src/lab5/game_world_gen_practice.py
+++ src/lab5/game_world_gen_practice.py
@@ -30,12 +30,47 @@ from lab2.cities_n_routes import get_randomly_spread_cities, get_routes
 
 ''' Create helper functions here '''
 
+###
+
+###
+def draw_cities(pygame_surface, black, cities):
+    """
+    > This function draws cities (as circles) on the surface provided. For example, when providing a list of cities, 
+    this will draw those cities at specified x and y cordinates
+    
+    :param pygame_surface: the surface for the game
+    :param black: The color of the circles
+    :param cities: a list of tuples of the cities
+    :return: void
+    """
+
+    for i, city in enumerate(cities): 
+        pygame.draw.circle(pygame_surface, black, (city[0], city[1]), 10)
+    pass
+
+def draw_routes(city_locations_dict, pygame_surface, black, routes):
+    """
+    > This function draw lines between two city cordinates. For example, route one is between city1 and city2.
+    The function will take the location of city1 and draw a line to city2 on the game surface.  
+    
+    :param city_locations_dict: the dictionary that provides which city corresponds to what location
+    :param pygame_surface: the surface for the game
+    :param black: The color of the lines
+    :param cities: a list of routes (city 1 to city 2)
+    :return: void
+    """
+
+    for i, route in enumerate(routes): 
+       pygame.draw.line(pygame_surface, black, city_locations_dict[route[0]], city_locations_dict[route[1]], 3)
+    pass
+
+
 if __name__ == "__main__":
     pygame.init()
     size = width, height = 640, 480
     black = 1, 1, 1
-
     screen = pygame.display.set_mode(size)
+    
     landscape = get_landscape(size)
     print("Created a landscape of size", landscape.shape)
     pygame_surface = pygame.surfarray.make_surface(landscape[:, :, :3]) 
@@ -46,6 +81,8 @@ if __name__ == "__main__":
     routes = []
 
     ''' Setup cities and routes in here'''
+    city_locations = get_randomly_spread_cities(size, len(city_names))
+    routes =  get_routes(city_names)
 
     city_locations_dict = {name: location for name, location in zip(city_names, city_locations)}
     random.shuffle(routes)
@@ -61,6 +98,10 @@ if __name__ == "__main__":
 
         ''' draw cities '''
 
+        draw_cities(pygame_surface, black, city_locations)
+
         ''' draw first 10 routes '''
+
+        draw_routes(city_locations_dict, pygame_surface, black, routes)
 
         pygame.display.flip()
--- src/lab5/landscape.py
+++ src/lab5/landscape.py
@@ -2,12 +2,13 @@ import matplotlib.pyplot as plt
 from perlin_noise import PerlinNoise
 import numpy as np
 
-def get_elevation(size):
+def get_elevation(size): # size = [680, 440]
     xpix, ypix = size
     elevation = np.array([])
     '''Play around with perlin noise to get a better looking landscape (This is required for the lab)'''
-
-    return elevation
+    noise = PerlinNoise(octaves=12) 
+    elevation = np.array([[noise([i/xpix, j/ypix]) for j in range(ypix)] for i in range (xpix)])
+    return elevation # elevation = [680, 440]
 
 def elevation_to_rgba(elevation):
     xpix, ypix = np.array(elevation).shape
