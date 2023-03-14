45b786deb9c39bc9882660cfad96ddcefd7e0860
Mason <masonwillman0@gmail.com>
2023-03-14 11:33:37 -0400

    Lab10 Submission

--- src/lab10/lab10.py
+++ src/lab10/lab10.py
@@ -10,7 +10,7 @@ from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("src/lab8/heart.csv")
+data = pd.read_csv("src/lab10/heart.csv")
 
 # Transform the categorical variables into dummy variables.
 print(data.head())
@@ -26,12 +26,24 @@ x_train, x_test, y_train, y_test = train_test_split(
 
 """ Train a sklearn model here. """
 
-sklearn_model = None
+# Creates a model based on K neighbors 
+sklearn_model = KNeighborsClassifier(n_neighbors = 12)
+# Trains the model on the train data
+sklearn_model.fit(x_train, y_train)
 
 # Accuracy
+# Tests to see if the trained model matches the results of the train data (test)
 print("Accuracy of model: {}\n".format(sklearn_model.score(x_test, y_test)))
 
 
 """ Improve the model by normalizing the input data. """
+# Normalizes the training data
+x_train = (x_train - x_train.min()) / (x_train.max() - x_train.min())
+
+# Normalizes the test data
+x_test = (x_test - x_test.min()) / (x_test.max() - x_test.min())
+
+# Refit the model based on normalized data
+sklearn_model.fit(x_train, y_train)
 
 print("Accuracy of improved model: {}\n".format(sklearn_model.score(x_test, y_test)))
